323e9ea01f4419d275a869f7085a0fd8203b7a4d
John Gilday <jgilday01@gmail.com>
2020-10-22 20:46:41 -0400

    supress yellow

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {YellowBox } from 'react-native';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+
+YellowBox.ignoreWarnings(["VirtualizedLists should never be nested"]);
 
 const store = ConfigureStore();
 
