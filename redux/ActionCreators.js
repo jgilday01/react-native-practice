68063152205ad782c5a4385b627d5899c294c2f8
John Gilday <jgilday01@gmail.com>
2020-10-27 08:12:17 -0400

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -148,12 +148,6 @@ export const postFavorite = campsiteId => dispatch => {
     }, 2000);
 }
 
-export const addFavorite = campsiteId => ({
-    type: ActionTypes.ADD_FAVORITE,
-    payload: campsiteId
-});
-
-
 export const postComment = (campsiteId, rating, author, text) => dispatch => {
     const newComment = {
         campsiteId,
@@ -172,4 +166,15 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
 export const addComment = comment => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comment
+});
+
+export const addFavorite = campsiteId => ({
+    type: ActionTypes.ADD_FAVORITE,
+    payload: campsiteId
+});
+
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
 });
